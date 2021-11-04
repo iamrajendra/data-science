66b0fbcb9db08254ae09935183e811bbb62a12e8
Raj Verma <rajendrarajaramv@gmail.com>
2021-11-04 09:33:56 +0530

    Remove the  duplicates from the list in python numbers = [2,2,4,6,3,4,6,1]

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# coding=utf-8
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-patient_name  = "john smith"
-patient_age  = 20
-patient_is_new  = True
-print(patient_name)
-print(patient_age)
-print(patient_is_new)
+numbers  = [2,2,4,6,3,4,6,1]
+uniques  = []
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print (uniques)
