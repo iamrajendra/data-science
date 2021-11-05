b9cf5e6c73d28cb6335600b966ffada2957c405c
Raj Verma <rajendrarajaramv@gmail.com>
2021-11-05 11:19:53 +0530

    Create a simple class with one variable,cons and one method

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-words = raw_input(">: ")
-words_list = words.split(" ")
-emoji_dict = {
-    ":)": "😃",
-    ":(": "☹️",
 
-}
-output_str = "";
-for word  in words_list:
-    output_str += emoji_dict.get(word,word)+" "
-print (output_str)
+class Person :
+    def __init__(self,name):
+       self. name  = name
+
+    def talk(self):
+
+       print ("Rajendra talks to "+self.name);
+
+rajendra  = Person("Kavita")
+rajendra.talk()
