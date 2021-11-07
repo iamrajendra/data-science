490f6443f1f87085f2bd5466671ec487e1c73267
Raj Verma <rajendrarajaramv@gmail.com>
2021-11-07 12:10:44 +0530

    facing problem in typeerror

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Function to take multiple arguments
-from random import Random
+import  openpyxl as xl
+# upload the spreedsheet
+wb  = xl.load_workbook('transections.xlsx')
+sheet = wb['Sheet1']
+cell  = sheet['a1']
+cell  = sheet.cell(1,1)
 
-
-class Coin:
-    def flip(self):
-        random  = Random()
-        face  = {
-            1:"Head",
-            2:"Tail"
-        }
-        num = random.randint(1,2)
-        return  face.get(num)
-
-coin  = Coin()
-print (coin.flip())
+for row  in range(1,sheet.max_row+1):
+    cell  = sheet.cell(row,3)
+    corrected_price  = cell.value*0.9
+    corrected_price_cell   = sheet.cell(cell,4)
+    corrected_price_cell.value  = corrected_price_cell
+wb.save("transection2.xlsx")
